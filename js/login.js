5ea7c9c08d1c03dec60fa1f742cbf5bd38effefe
Mahir-H07 <ttxmahir@gmail.com>
2024-09-18 21:25:29 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+// console.log("button clicking file added");
+
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('login button click');
+
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
