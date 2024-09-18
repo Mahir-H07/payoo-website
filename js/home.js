b41dbcc2cd086537e63801843db78ba7296540d3
Mahir-H07 <ttxmahir@gmail.com>
2024-09-18 22:34:34 +0600

    button click handaler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home added');
+
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault()
+   
+
+
+    const addMoney = document.getElementById('input-add-money').value;
+    console.log(addMoney);
+
+    const pinNumberInput = document.getElementById('Input-pin-number').value;
+    console.log(pinNumberInput);
+})
