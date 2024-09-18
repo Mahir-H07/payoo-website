60ed313cce25b337946886cb478afad0cf64f08a
Mahir-H07 <ttxmahir@gmail.com>
2024-09-18 22:11:19 +0600

    home part added

--- js/login.js
+++ js/login.js
@@ -3,9 +3,18 @@
 
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login button click');
+    // console.log('login button click');
 
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value
+    console.log(phoneNumber,  pinNumber)
+
+    if (phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong number')
+    }
 })
